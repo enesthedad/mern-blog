9afca4784c3bd9622135fdd759e13b3a1575a87e
enesthedad <robinthelevi@gmail.com>
2024-03-04 19:16:32 +0300

    create connection to the database and create environment variables

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const connectToDb = async () =>
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(console.log("connected to the db"))
+    .catch((err) => console.log(err));
 const app = express();
 
 app.listen(3000, () => {
   console.log("server is on 3000");
+  connectToDb();
 });
