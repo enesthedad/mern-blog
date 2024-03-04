809ea5b4aa36794a6e572ad15667ff6e9dd84cb4
enesthedad <robinthelevi@gmail.com>
2024-03-04 17:07:58 +0300

    create pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+import { Projects } from "./pages/Projects";
+import { Dashboard } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <SignUp />,
+  },
+  {
+    path: "/",
+    element: <Projects />,
+  },
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+
+  {
+    path: "*",
+    element: (
+      <div className="h-screen w-full bg-stone-900 text-white flex justify-center items-center">
+        {" "}
+        <h1 className="text-[24px] font-bold">404 page not found</h1>{" "}
+      </div>
+    ),
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <div className=" p-10">app</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
