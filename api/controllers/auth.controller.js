cf2313fbd4e1a529e16bad9428042c3e0f69270f
enesthedad <robinthelevi@gmail.com>
2024-03-06 20:07:13 +0300

    create login api route and controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const isUserExists = await User.findOne({ username });
@@ -36,5 +36,33 @@ export const signup = async (req, res, next) => {
     });
   } catch (error) {
     return next(errorHandler(401, "Signup error please try again!"));
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === " " || password === " ") {
+    return next(errorHandler(400, "All fields are required!"));
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(400, "username doesn't exists!"));
+    }
+    const comparePasswords = bcrypt.compareSync(password, user.password);
+    if (!comparePasswords) {
+      return next(400, "incorrect password!!");
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
+    const { password: pass, ...rest } = user._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+    return user;
+  } catch (error) {
+    return next(error);
   }
 };
